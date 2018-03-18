d9a5503d850104710d9144724d8b4b5ef1920680
Mark Liu <markisus@gmail.com>
2018-03-17 17:22:38 -0700

    Most basic functionality implemented

--- include/BasicRelation.h
+++ include/BasicRelation.h
@@ -5,9 +5,35 @@
 #include <tuple>
 #include <iostream>
 #include <functional>
+#include <random>
+#include <ostream>
+
 #include "GenScatterHierarchy.h"
 
 namespace vapid_db {
+
+template <typename Idx, typename T>
+struct TupleDumper;
+
+template <uint16_t idx, typename... Ts>
+struct TupleDumper<std::integral_constant<uint16_t, idx>, std::tuple<Ts...>> {
+  static void dump(std::basic_ostream<char>& ss, const std::tuple<Ts...>& t) {
+      ss << std::get<idx>(t) << ", ";
+      TupleDumper<std::integral_constant<uint16_t,idx+1>, std::tuple<Ts...>>::dump(ss, t);
+    }
+};
+
+template <typename... Ts>
+struct TupleDumper<std::integral_constant<uint16_t, std::tuple_size<std::tuple<Ts...>>::value - 1>, std::tuple<Ts...>> {
+  static void dump(std::basic_ostream<char>& ss, const std::tuple<Ts...>& t) {
+      ss << std::get< std::tuple_size<std::tuple<Ts...>>::value -1>(t);
+    }
+};
+
+template <typename T>
+void dump_tuple(std::basic_ostream<char>& ss, const T& t) {
+  TupleDumper<std::integral_constant<uint16_t, 0>, T>::dump(ss, t);
+}
 
 template <typename DataType>
 struct Column {
@@ -92,13 +118,22 @@ class BasicRelation : public GenScatterHierarchy<Column, Ts...> {
     iterate_columns_impl(op, TypeList<Column<Ts>...>{});
   }
 
+  // if optional end = size_t(-1), it is set to the size of the list
+  // operates within the range [begin, end)
+  // returns the index of last write or end if no swaps
   size_t swap_matches_to_back(
-      std::function<bool(const std::tuple<Ts...>&)> predicate) {
-    size_t write_head = size();
+      std::function<bool(const std::tuple<Ts...>&)> predicate,
+      size_t begin = 0, size_t end = size_t(-1)) {
+    size_t write_head = (end == size_t(-1) ? size() : end);
+
+    if (begin >= end) {
+      return end;
+    }
+
     size_t read_head = write_head;
-    while (read_head != 0) {
+    while (read_head != begin) {
       --read_head;
-      if (predicate((*this)[read_head])) {
+      if (predicate(this->as_tuple(read_head))) {
         --write_head;
         swap_indices(read_head, write_head);
       }
@@ -116,7 +151,7 @@ class BasicRelation : public GenScatterHierarchy<Column, Ts...> {
     size_t read_head = 0;
     size_t the_size = size();
     while (read_head < the_size) {
-      if (predicate((*this)[read_head])) {
+      if (predicate(this->as_tuple(read_head))) {
         write_head;
         swap_indices(read_head, write_head);
       }
@@ -124,11 +159,103 @@ class BasicRelation : public GenScatterHierarchy<Column, Ts...> {
     return write_head;
   }
 
-
-  std::tuple<Ts...> operator[](size_t idx) {
+  std::tuple<Ts...> as_tuple(size_t idx) {
     std::tuple<Ts...> result;
     fill_tuple(&result, idx, std::integral_constant<uint16_t, 0>{});
     return result;
+  }
+
+  // quick sort the elements at indices [being, end)
+  void quick_sort(std::mt19937& rng,
+                  size_t begin = 0,
+                  size_t end = size_t(-1)) {
+    // default comparator
+    auto comparator = [](
+        const std::tuple<Ts...>& a, const std::tuple<Ts...>& b) {
+        return a < b;
+      };
+
+    quick_sort(rng, comparator, begin, end);
+  }
+  
+
+  // quick sort the elements at indices [being, end)
+  void quick_sort(std::mt19937& rng,
+                  std::function<bool(
+                      const std::tuple<Ts...>&, const std::tuple<Ts...>&)
+                  > comparator,
+                  size_t begin = 0,
+                  size_t end = size_t(-1)) {
+    if (end == size_t(-1)) {
+      end = size();
+    }
+
+    if (begin + 1 >= end) {
+      return;
+    }
+
+    std::uniform_int_distribution<size_t> uni(begin, end-1);
+
+    // [begin ... | pivot | ... end ]
+    size_t pivot_idx = uni(rng);
+
+    swap_indices(begin, pivot_idx);
+    // [pivot | begin ... | ... | ... end ]
+
+    std::tuple<Ts...> pivot_element = this->as_tuple(begin);
+    auto Is_gte_pivot = [pivot_element, comparator](const std::tuple<Ts...>& t) {
+      return !comparator(t, pivot_element);
+    };
+
+    size_t greater_than_pivot_begin = swap_matches_to_back(
+        Is_gte_pivot, begin+1, end);
+
+    // [pivot | lt_pivot ... | greater_than_pivot_begin ... ]
+
+    // swap pivot into its correct location
+    swap_indices(greater_than_pivot_begin-1, begin);
+    // [ lt_pivot ... | pivot| greater_than_pivot_begin ... ]
+
+    // quick sort the two parts
+    quick_sort(rng, comparator, greater_than_pivot_begin, end);
+    quick_sort(rng, comparator, begin, greater_than_pivot_begin-1);
+  }
+
+  
+  template <uint16_t idx>
+  void quick_sort_by_field(std::mt19937& rng,
+                           std::function<bool(
+                               typename Head<typename Pop<idx, TypeList<Ts...>>::type>::type,
+                               typename Head<typename Pop<idx, TypeList<Ts...>>::type>::type)>
+                           comparator,
+                           size_t begin = 0,
+                           size_t end = size_t(-1)) {
+    auto tuple_comparator = [comparator](
+        const std::tuple<Ts...>& a, const std::tuple<Ts...>& b) {
+      return comparator(std::get<idx>(a), std::get<idx>(b));
+    };
+    quick_sort(rng, comparator, begin, end);
+  }
+
+  void dump(std::basic_ostream<char>& ss) {
+    constexpr size_t MAX_NUM_ELEMENTS_TO_PRINT = 25;
+    size_t num_elements_to_print = size();
+    bool too_many_elements = false;
+    if (num_elements_to_print > MAX_NUM_ELEMENTS_TO_PRINT) {
+      num_elements_to_print = MAX_NUM_ELEMENTS_TO_PRINT;
+      too_many_elements = true;
+    }
+    ss << "BasicRelation {\n";
+    for (size_t i = 0; i < num_elements_to_print; ++i) {
+      std::tuple<Ts...> t = this->as_tuple(i);
+      ss << "\t";
+      dump_tuple(ss, t);
+      ss << std::endl;
+    }
+    if (size() > MAX_NUM_ELEMENTS_TO_PRINT) {
+      ss << "\t..." << std::endl;
+    }
+    ss << "}";
   }
 
  private:
--- scratch.cc
+++ scratch.cc
@@ -8,17 +8,27 @@ int main(int argc, char *argv[])
     myRelation.insert(5.02, "helloooo", 3);
     myRelation.insert(6.23, "goodbye", 2);
     myRelation.insert(3.14, "goodbye", 1000);
-    myRelation.swap_indices(0,1);
-    myRelation.overwrite(0, std::make_tuple(4.099, "fuck", 1));
-    myRelation.swap_matches_to_back(
+    myRelation.insert(444.14, "eh", 1000);
+    myRelation.insert(-2.14, "eh", 1000);
+    myRelation.insert(5.04, "helloooo", 3);
+    myRelation.insert(6.25, "goodbye", 2);
+    myRelation.insert(3.18, "goodbye", 1000);
+    myRelation.insert(444.19, "eh", 1000);
+    myRelation.insert(-2.134, "eh", 1000);
+
+    myRelation.overwrite(0, std::make_tuple(4.099, "fudge", 1));
+    /*
+    myRelation.swap_indices(0,5);
+    size_t result = myRelation.swap_matches_to_back(
         [](const std::tuple<double, std::string, int>& t) {
-          return std::get<1>(t) == "fuck";
-        });
-    myRelation.resize(2);
-    for (int i = 0; i < myRelation.size(); ++i) {
-      double a; std::string b; int c;
-      std::tie(a, b, c) = myRelation[i];
-      std::cout << a << ", " << b << ", " << c << std::endl;
-    }
+          return std::get<0>(t) >= 5.04;
+        },        1, myRelation.size());
+    std::cout << "matches start at " << result << std::endl;
+    */
+    myRelation.resize(myRelation.size()-1);
+    std::random_device rd;     // only used once to initialise (seed) engine
+    std::mt19937 rng(rd());
+    myRelation.quick_sort(rng);
+    myRelation.dump(std::cout);
     return 0;
 }
